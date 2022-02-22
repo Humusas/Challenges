9bd1ff268e88c0e2b6ed0d1103bbbb9e1b944d20
sd <s.pauliukenas@gmail.com>
2022-02-22 20:54:31 +0000

    started Pointers challenge

--- CodeCracker/PointersStructs.cpp
+++ CodeCracker/PointersStructs.cpp
@@ -5,23 +5,33 @@ int main()
 {
 //=====================TASK1==============================
 //1. Consider the following struct:
-	/*
+	
 	struct House
 	{
-		bool hasPool; //true or false
-		int totalRooms;
-		short totalPeople;
+		bool hasPool;		//true or false
+		int totalRooms;		//int variable
+		short totalPeople;	//int variable
 	};
 	
-	a.Create a pointer to House and nullify it
-	b. Create an instance of House on the heap 
-	c. Add values to the newly created object 
-	d. Destroy the House object
-	*/
+	//a.Create a pointer to House and nullify it
+	//b. Create an instance of House on the heap 
+	//c. Add values to the newly created object 
+	//d. Destroy the House object
+	
+	House Regular;
 
 //========================================================
-	
-	//nullptr House*;
-	
+
+//a.	
+
+	House* Regular{ false, 0 ,0 };
+//b.
+	->House;
+
+//c.
+
+
+//d.
+	~;
 }
 
